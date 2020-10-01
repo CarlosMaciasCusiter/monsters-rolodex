204cff37f1f908207a1261c3d0e3821c2a515d46
Carlos Macias Cusiter <CarlosMaciasCusiter@gmail.com>
2020-10-01 16:24:55 -0700

    Updates

---
+++ precache-manifest.ae859ef2533cf63b3fdb116cdc738ead.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4a9b9fce8fec1cb3fe9ab225f0edfbe9",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "b1585cb3dfc1baa5f3a9",
+    "url": "/monsters-rolodex/static/css/main.824fde50.chunk.css"
+  },
+  {
+    "revision": "efe1924630397efd073d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.b4309f00.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b1585cb3dfc1baa5f3a9",
+    "url": "/monsters-rolodex/static/js/main.4e94ee07.chunk.js"
+  },
+  {
+    "revision": "9b59177dc23ec2a3d1f7",
+    "url": "/monsters-rolodex/static/js/runtime-main.fa9629a0.js"
+  }
+]);
